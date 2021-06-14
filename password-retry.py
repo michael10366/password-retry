94018357d2a8019f3f3be3c0a2e4b5c9042c0a2a
michael wu <michael10366@outlook.com>
2021-06-14 19:19:27 +0800

    'finish'

--- password-retry.py
+++ password-retry.py
@@ -1,10 +1,14 @@
 password = 'a123456'
 i = 3 #剩餘機會
 while i > 0:
+	i = i - 1
 	userpassword = input('請輸入密碼')
 	if userpassword == password:
 		print('登入成功')
 		break
 	else:
-		i = i - 1
-		print('密碼錯誤! 還有', i, '次機會')
+		print('密碼錯誤!')
+		if i > 0:
+			print('還有', i, '次機會')
+		else:
+			print('帳號鎖定')
